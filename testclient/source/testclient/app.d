cb4a4a8d076a7ce1885fad9b3c6d81910467ab69
Cameron Ross <elpenguino@gmail.com>
2024-03-22 13:57:28 -0300

    update testclient for removal of old template interface, update deps

--- testclient/source/testclient/app.d
+++ testclient/source/testclient/app.d
@@ -1,5 +1,7 @@
 module testclient.app;
 
+import std.algorithm;
+import std.json;
 import std.range;
 import std.stdio;
 import vibe.core.core;
@@ -8,18 +10,73 @@ import vibe.stream.operations;
 import vibe.stream.stdio;
 import vibe.stream.tls;
 import vibe.stream.wrapper;
+import virc;
 import virc.client;
 
-mixin template Client() {
-	import std.stdio : writefln, writef;
-	string currentChannel;
-	string[] channelsToJoin;
-	void onMessage(const User user, const Target target, const Message msg, const MessageMetadata metadata) @safe {
+interface IStream : Output {
+	ubyte[] readLine();
+	bool empty();
+}
+
+class WrapStream : IStream {
+	Stream stream;
+	this(Stream stream) nothrow {
+		this.stream = stream;
+	}
+	override void put(char c) {
+		stream.write([c]);
+	}
+	override ubyte[] readLine() {
+		return stream.readLine();
+	}
+	override bool empty() {
+		return stream.empty;
+	}
+}
+class WrapConnection : IStream {
+	TCPConnection connection;
+	this(TCPConnection conn) nothrow {
+		connection = conn;
+	}
+	override void put(char c) {
+		connection.write([c]);
+	}
+	override ubyte[] readLine() {
+		return connection.readLine();
+	}
+	override bool empty() {
+		return connection.empty;
+	}
+}
+
+string currentChannel;
+string[] channelsToJoin;
+void autoJoinChannel(string chan) @safe {
+	channelsToJoin ~= chan;
+}
+
+auto runClient(JSONValue settings, IStream stream) {
+	import std.typecons;
+	auto client = ircClient(stream, NickInfo(settings["nickname"].str, settings["identd"].str, settings["real name"].str));
+	void writeLine(string line) {
+		import std.string : toLower;
+		if (line.startsWith("/")) {
+			auto split = line[1..$].splitter(" ");
+			switch(split.front.toLower()) {
+				default:
+					write(line[1..$]);
+					break;
+			}
+		} else {
+			client.msg(currentChannel, line);
+		}
+	}
+	client.onMessage = (const User user, const Target target, const Message msg, const MessageMetadata metadata) @safe {
 		if (msg.isCTCP) {
 			if (msg.ctcpCommand == "ACTION") {
 				writefln("<%s> * %s %s", metadata.time, user, msg.ctcpArgs);
 			} else if (msg.ctcpCommand == "VERSION") {
-				ctcpReply(Target(user), "VERSION", "virc-testclient");
+				client.ctcpReply(Target(user), "VERSION", "virc-testclient");
 			} else {
 				writefln("<%s> [%s:%s] %s", metadata.time, user, msg.ctcpCommand, msg.ctcpArgs);
 			}
@@ -28,53 +85,53 @@ mixin template Client() {
 		} else if (msg.isReplyable) {
 			writefln("<%s> <%s:%s> %s", metadata.time, user.nickname, target, msg.msg);
 		}
-	}
+	};
 
-	void onJoin(const User user, const Channel channel, const MessageMetadata metadata) @safe {
+	client.onJoin = (const User user, const Channel channel, const MessageMetadata metadata) @safe {
 		writefln("<%s> *** %s joined %s", metadata.time, user, channel);
 		currentChannel = channel.name;
-	}
+	};
 
-	void onPart(const User user, const Channel channel, const string message, const MessageMetadata metadata) @safe {
+	client.onPart = (const User user, const Channel channel, const string message, const MessageMetadata metadata) @safe {
 		writefln("<%s> *** %s parted %s: %s", metadata.time, user, channel, message);
-	}
+	};
 
-	void onQuit(const User user, const string message, const MessageMetadata metadata) @safe {
+	client.onQuit = (const User user, const string message, const MessageMetadata metadata) @safe {
 		writefln("<%s> *** %s quit IRC: %s", metadata.time, user, message);
-	}
+	};
 
-	void onNick(const User user, const User newname, const MessageMetadata metadata) @safe {
+	client.onNick = (const User user, const User newname, const MessageMetadata metadata) @safe {
 		writefln("<%s> *** %s changed name to %s", metadata.time, user, newname);
-	}
+	};
 
-	void onKick(const User user, const Channel channel, const User initiator, const string message, const MessageMetadata metadata) @safe {
+	client.onKick = (const User user, const Channel channel, const User initiator, const string message, const MessageMetadata metadata) @safe {
 		writefln("<%s> *** %s was kicked from %s by %s: %s", metadata.time, user, channel, initiator, message);
-	}
+	};
 
-	void onLogin(const User user, const MessageMetadata metadata) @safe {
+	client.onLogin = (const User user, const MessageMetadata metadata) @safe {
 		writefln("<%s> *** %s logged in", metadata.time, user);
-	}
+	};
 
-	void onLogout(const User user, const MessageMetadata metadata) @safe {
+	client.onLogout = (const User user, const MessageMetadata metadata) @safe {
 		writefln("<%s> *** %s logged out", metadata.time, user);
-	}
+	};
 
-	void onAway(const User user, const string message, const MessageMetadata metadata) @safe {
+	client.onOtherUserAwayReply = (const User user, const string message, const MessageMetadata metadata) @safe {
 		writefln("<%s> *** %s is away: %s", metadata.time, user, message);
-	}
+	};
 
-	void onBack(const User user, const MessageMetadata metadata) @safe {
+	client.onBack = (const User user, const MessageMetadata metadata) @safe {
 		writefln("<%s> *** %s is no longer away", metadata.time, user);
-	}
+	};
 
-	void onTopic(const User user, const Channel channel, const string topic, const MessageMetadata metadata) @safe {
+	client.onTopicChange = (const User user, const Channel channel, const string topic, const MessageMetadata metadata) @safe {
 		writefln("<%s> *** %s changed topic on %s to %s", metadata.time, user, channel, topic);
-	}
+	};
 
-	void onMode(const User user, const Target target, const ModeChange mode, const MessageMetadata metadata) @safe {
+	client.onMode = (const User user, const Target target, const ModeChange mode, const MessageMetadata metadata) @safe {
 		writefln("<%s> *** %s changed modes on %s: %s", metadata.time, user, target, mode);
-	}
-	void onWhois(const User user, const WhoisResponse whoisResponse) @safe {
+	};
+	client.onWhois = (const User user, const WhoisResponse whoisResponse) @safe {
 		writefln("%s is %s@%s (%s)", user, whoisResponse.username, whoisResponse.hostname, whoisResponse.realname);
 		if (whoisResponse.isOper) {
 			writefln("%s is an IRC operator", user);
@@ -97,37 +154,14 @@ mixin template Client() {
 		if (!whoisResponse.connectedTo.isNull) {
 			writefln("%s is connected to %s", user, whoisResponse.connectedTo);
 		}
-	}
-	void onConnect() @safe {
+	};
+	client.onConnect = () @safe {
 		foreach (channel; channelsToJoin) {
-			join(channel);
+			client.join(channel);
 		}
-	}
-	void writeLine(string line) {
-		import std.string : toLower;
-		if (line.startsWith("/")) {
-			auto split = line[1..$].splitter(" ");
-			switch(split.front.toLower()) {
-				default:
-					write(line[1..$]);
-					break;
-			}
-		} else {
-			msg(currentChannel, line);
-		}
-	}
-	void autoJoinChannel(string chan) @safe {
-		channelsToJoin ~= chan;
-	}
-}
-
-import std.json;
-auto runClient(T)(JSONValue settings, ref T stream) {
-	import std.typecons;
-	auto output = refCounted(streamOutputRange(stream));
-	auto client = ircClient!Client(output, NickInfo(settings["nickname"].str, settings["identd"].str, settings["real name"].str));
+	};
 	foreach (channel; settings["channels to join"].arrayNoRef) {
-		client.autoJoinChannel(channel.str);
+		autoJoinChannel(channel.str);
 	}
 
 	void readIRC() nothrow {
@@ -144,7 +178,7 @@ auto runClient(T)(JSONValue settings, ref T stream) {
 			auto standardInput = new StdinStream;
 			while (true) {
 				auto str = cast(string)readLine(standardInput);
-				client.writeLine(str);
+				writeLine(str);
 			}
 		} catch (Exception e) {
 			assert(0, e.msg);
@@ -171,9 +205,9 @@ int main() {
 				writeln("SSL connection failed!");
 				return 1;
 			}
-			return runClient(settings, stream);
+			return runClient(settings, new WrapStream(stream));
 		} else {
-			return runClient(settings, conn);
+			return runClient(settings, new WrapConnection(conn));
 		}
 	} else {
 		writeln("No settings file found");
